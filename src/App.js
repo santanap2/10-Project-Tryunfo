844a7f92f7f2e0f74cdd07fe80162a923a8bf03c
PedroSantana <phsantana99@gmail.com>
2022-07-31 19:08:53 -0300

    requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Form from './components/Form';
 import './index.css';
+import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Tryunfo</h1>
+        <h1 className="main-title">Tryunfo</h1>
+
         <Form
           cardName=""
           cardDescription=""
@@ -21,6 +23,18 @@ class App extends React.Component {
           onInputChage={ () => { } }
           onSaveButtonClick={ () => { } }
         />
+
+        <Card
+          cardName="Stephen Curry"
+          cardDescription="Melhor arremessador de 3 pontos da história da NBA."
+          cardAttr1="Aremesso: 98"
+          cardAttr2="Dunk: 70"
+          cardAttr3="Movimentação: 92"
+          cardImage="https://istoe.com.br/wp-content/uploads/2022/05/stephen-curry-418x235.jpg"
+          cardRare="muito raro"
+          cardTrunfo
+        />
+
       </div>
     );
   }
---
+++ src/components/Card.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Card extends Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <div className="card">
+
+        { cardTrunfo ? <p data-testid="trunfo-card" id="trunfo">Super Trunfo</p> : '' }
+
+        <div className="title-rarity">
+
+          <h3
+            data-testid="name-card"
+            className="card-name"
+          >
+            { cardName }
+          </h3>
+
+          <p
+            data-testid="rare-card"
+            className="rarity"
+          >
+            { cardRare }
+          </p>
+
+        </div>
+
+        <img
+          data-testid="image-card"
+          className="card-img"
+          src={ cardImage }
+          alt={ cardName }
+        />
+
+        <p
+          data-testid="description-card"
+        >
+          {cardDescription}
+        </p>
+
+        <div className="attr-div">
+          <p
+            data-testid="attr1-card"
+            className="attr"
+          >
+            {cardAttr1}
+          </p>
+
+          <p
+            data-testid="attr2-card"
+            className="attr"
+          >
+            {cardAttr2}
+          </p>
+
+          <p
+            data-testid="attr3-card"
+            className="attr"
+          >
+            {cardAttr3}
+          </p>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: propTypes.string.isRequired,
+  cardDescription: propTypes.string.isRequired,
+  cardAttr1: propTypes.string.isRequired,
+  cardAttr2: propTypes.string.isRequired,
+  cardAttr3: propTypes.string.isRequired,
+  cardImage: propTypes.string.isRequired,
+  cardRare: propTypes.string.isRequired,
+  cardTrunfo: propTypes.bool.isRequired,
+};
+
+export default Card;
